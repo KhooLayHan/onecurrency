8f9a90c9ef20aa1b029a57afe1dac88a842eca4b
KhooLayHan <khoolayhan@github.com>
2026-02-22 12:23:06 +0800

    fix: updated schema field for db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,10 @@ import "dotenv/config";
 import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
-// import * as schema from "./db/schema";
+import { kycStatuses } from "./db/schema/kyc-statuses";
+import { networks } from "./db/schema/networks";
+import { transactionStatuses } from "./db/schema/transaction-statuses";
+import { transactionTypes } from "./db/schema/transaction-types";
 import { env } from "./env";
 
 neonConfig.webSocketConstructor = ws;
@@ -10,6 +13,11 @@ neonConfig.webSocketConstructor = ws;
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 export const db = drizzle({
   client: pool,
-  // schema,
+  schema: {
+    ...kycStatuses,
+    ...transactionStatuses,
+    ...transactionTypes,
+    ...networks,
+  },
   logger: true,
 });
