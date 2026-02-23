de831d3083dba67b2dadc2e470bf16ebd0af6493
KhooLayHan <khoolayhan@github.com>
2026-02-23 22:20:39 +0800

    feat: updated relations.ts file to define relations

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,11 +2,17 @@ import "dotenv/config";
 import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
+import { accounts } from "./db/schema/accounts";
 import { kycStatuses } from "./db/schema/kyc-statuses";
 import { networks } from "./db/schema/networks";
 import { relations } from "./db/schema/relations";
+import { roles } from "./db/schema/roles";
+import { sessions } from "./db/schema/sessions";
 import { transactionStatuses } from "./db/schema/transaction-statuses";
 import { transactionTypes } from "./db/schema/transaction-types";
+import { userRoles } from "./db/schema/user-roles";
+import { users } from "./db/schema/users";
+import { verifications } from "./db/schema/verifications";
 import { env } from "./env";
 
 neonConfig.webSocketConstructor = ws;
@@ -20,6 +26,12 @@ export const db = drizzle({
     transactionStatuses,
     transactionTypes,
     networks,
+    roles,
+    userRoles,
+    users,
+    sessions,
+    accounts,
+    verifications,
     relations,
   },
   logger: env.NODE_ENV !== "production",
--- backend/src/db/schema/relations.ts
+++ backend/src/db/schema/relations.ts
@@ -1,3 +1,59 @@
 import { defineRelations } from "drizzle-orm";
+import { accounts } from "./accounts";
+import { kycStatuses } from "./kyc-statuses";
+import { roles } from "./roles";
+import { sessions } from "./sessions";
+import { userRoles } from "./user-roles";
+import { users } from "./users";
+import { verifications } from "./verifications";
 
-export const relations = defineRelations({});
+export const relations = defineRelations(
+  {
+    kycStatuses,
+    roles,
+    userRoles,
+    users,
+    sessions,
+    accounts,
+    verifications,
+  },
+  (r) => ({
+    users: {
+      kycStatus: r.one.kycStatuses({
+        from: r.users.kycStatusId,
+        to: r.kycStatuses.id,
+      }),
+      sessions: r.many.sessions(),
+      accounts: r.many.accounts(),
+      userRoles: r.many.userRoles(),
+    },
+    sessions: {
+      user: r.one.users({
+        from: r.sessions.userId,
+        to: r.users.id,
+      }),
+    },
+    accounts: {
+      account: r.one.users({
+        from: r.accounts.userId,
+        to: r.users.id,
+      }),
+    },
+    kycStatuses: {
+      users: r.many.users(),
+    },
+    roles: {
+      userRoles: r.many.userRoles(),
+    },
+    userRoles: {
+      user: r.one.users({
+        from: r.userRoles.userId,
+        to: r.users.id,
+      }),
+      role: r.one.roles({
+        from: r.userRoles.roleId,
+        to: r.roles.id,
+      }),
+    },
+  })
+);
