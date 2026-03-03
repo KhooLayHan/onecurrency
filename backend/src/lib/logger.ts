6e997f50345fa4a10b229df634f52c2a67fd1d9f
KhooLayHan <khoolayhan@github.com>
2026-03-04 03:47:48 +0800

    chore: pnpm install

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,4 +1,4 @@
-import { createBaseLogger } from "../../../packages/common/src/lib/logger"
+import { createBaseLogger } from "../../../packages/common/src/lib/logger";
 import { env } from "@/src/env";
 
 export const logger = createBaseLogger({
