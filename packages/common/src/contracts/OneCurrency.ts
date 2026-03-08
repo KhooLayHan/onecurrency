cd76e803fb4aed2108557d9dec5414e0426b2528
KhooLayHan <khoolayhan@github.com>
2026-03-09 06:44:43 +0800

    temp: remove formating for frontend for now, and added onecurrency address

--- packages/common/src/contracts/OneCurrency.ts
+++ packages/common/src/contracts/OneCurrency.ts
@@ -1,13 +1,13 @@
-export const OneCurrencyABI =[
+export const OneCurrencyABI = [
   "function mint(address to, uint256 amount) public",
   "function burn(uint256 amount) public",
   "function balanceOf(address account) public view returns (uint256)",
   "function isBlacklisted(address account) public view returns (bool)",
-  
+
   // Events
   "event Transfer(address indexed from, address indexed to, uint256 value)",
   "event AccountBlacklisted(address indexed account)",
-  "event AccountUnblacklisted(address indexed account)"
+  "event AccountUnblacklisted(address indexed account)",
 ] as const;
 
-export const ONECURRENCY_ADDRESS = "0xYourDeployedContractAddressHere";
+export const ONECURRENCY_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
