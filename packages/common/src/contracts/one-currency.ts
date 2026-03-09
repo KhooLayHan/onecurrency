d11ef72da33565f079bebabee5c0778eb219be90
KhooLayHan <khoolayhan@github.com>
2026-03-09 12:18:57 +0800

    chore: added viem dependencies for packages.common and fixed to use parseAbi()

--- packages/common/src/contracts/one-currency.ts
+++ packages/common/src/contracts/one-currency.ts
@@ -1,13 +1,26 @@
-export const OneCurrencyABI = [
+import { type Abi, parseAbi } from "viem";
+
+export const OneCurrencyABI: Abi = parseAbi([
   "function mint(address to, uint256 amount) public",
   "function burn(uint256 amount) public",
   "function balanceOf(address account) public view returns (uint256)",
-  "function isBlacklisted(address account) public view returns (bool)",
 
-  // Events
+  "function isBlacklisted(address account) public view returns (bool)",
+  "function blacklistAccount(address account) public",
+  "function unBlacklistAccount(address account) public",
+
   "event Transfer(address indexed from, address indexed to, uint256 value)",
   "event AccountBlacklisted(address indexed account)",
   "event AccountUnblacklisted(address indexed account)",
-] as const;
 
-export const ONECURRENCY_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+  "function DEFAULT_ADMIN_ROLE() public view returns (bytes32)",
+  "function MINTER_ROLE() public view returns (bytes32)",
+  "function BLACKLIST_ROLE() public view returns (bytes32)",
+  "function hasRole(bytes32 role, address account) public view returns (bool)",
+
+  "error AccessControlUnauthorizedAccount(address account, bytes32 neededRole)",
+  "error BlacklistedAccount(address account)",
+]);
+
+export const ONECURRENCY_ADDRESS: string =
+  "0x5fbdb2315678afecb367f032d93f642f64180aa3";
