687ae3dceed1553536374666bb0dcc81641ec8cc
KhooLayHan <khoolayhan@github.com>
2026-03-15 20:06:12 +0800

    fix: added terrible code for stripe paymentl and satisfy coderabbit

--- backend/src/services/stripe.service.ts
+++ backend/src/services/stripe.service.ts
@@ -12,8 +12,10 @@ export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
  * 100 USD = 10_000 cents = 100 * 10^18 Wei
  */
 export function calculateTokenAmountWei(amountCents: number): string {
-  const POWER_OF_16 = 16;
+  const POWER_OF_18 = 18;
+  const PRECENTAGE = 100;
 
-  const WEI_PER_CENT = BigInt(10) ** BigInt(POWER_OF_16); // 1e18 / 100
+  const WEI_PER_DOLLAR = BigInt(10) ** BigInt(POWER_OF_18); // 1 ONE = 10^18 Wei
+  const WEI_PER_CENT = WEI_PER_DOLLAR / BigInt(PRECENTAGE);
   return (BigInt(amountCents) * WEI_PER_CENT).toString();
 }
