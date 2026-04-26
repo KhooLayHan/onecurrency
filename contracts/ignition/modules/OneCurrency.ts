3e27de3b6126a7f7c63822024847bf8d6a79cfdb
KhooLayHan <khoolayhan@github.com>
2026-04-26 17:53:50 +0800

    refactor: migreated to use ignition modules now

--- contracts/ignition/modules/OneCurrency.ts
+++ contracts/ignition/modules/OneCurrency.ts
@@ -2,7 +2,9 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const MAX_SUPPLY_TOKENS = 1_000_000n;
 const TOKEN_DECIMALS = 18n;
-const MAX_SUPPLY_WEI = MAX_SUPPLY_TOKENS * 10n ** TOKEN_DECIMALS;
+const MULTIPLIER_10 = 10n;
+
+const MAX_SUPPLY_WEI = MAX_SUPPLY_TOKENS * MULTIPLIER_10 ** TOKEN_DECIMALS;
 
 export default buildModule("OneCurrencyModule", (m) => {
   const defaultAdmin = m.getAccount(0);
