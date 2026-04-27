d5ef7bc09d7e2e43c7376c8a872e56ea71f2a16c
KhooLayHan <khoolayhan@github.com>
2026-04-28 04:09:36 +0800

    fix: ci build issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
-import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: path.resolve(__dirname, "../"),
-  },
 };
 
 export default nextConfig;
